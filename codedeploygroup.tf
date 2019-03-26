0d19d9c423f3e317eb26187c89cbe2dd2b33c560
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-03-26 10:19:30 +0000

    init

--- codedeploygroup.tf
+++ codedeploygroup.tf
@@ -5,7 +5,7 @@ resource "aws_codedeploy_deployment_group" "example" {
   app_name              = "${aws_codedeploy_app.app.name}"
   deployment_group_name = "${var.environment}-${aws_codedeploy_app.app.name}-deployment-group"
   service_role_arn      = "${aws_iam_role.role.arn}"
-  autoscaling_groups    = "${var.autoscaling_groups}"
+  autoscaling_groups    = ["${var.autoscaling_groups}"]
 
 
 
