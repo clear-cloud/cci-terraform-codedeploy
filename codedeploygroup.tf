bd694949494dcb07bc8153082784258c6a4a9c49
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-09-30 13:04:32 +0000

    Changes to facilitate Blue-Green style deployments

--- codedeploygroup.tf
+++ codedeploygroup.tf
@@ -6,4 +6,22 @@ resource "aws_codedeploy_deployment_group" "example" {
   deployment_group_name = "${var.environment}-${aws_codedeploy_app.app.name}-deployment-group"
   service_role_arn      = "${aws_iam_role.role.arn}"
   autoscaling_groups    = ["${var.autoscaling_groups}"]
+
+  deployment_style {
+    deployment_option = "${var.deployment_option}"
+    deployment_type   = "${var.deployment_type}"
+  }
+
+  deployment_ready_option {
+    action_on_timeout    = "${var.action_on_timeout}"
+    wait_time_in_minutes = "${var.wait_time_in_minutes}"
+  }
+
+  green_fleet_provisioning_option {
+    action = "${var.green_fleet_provisioning_option_action}"
+  }
+
+  terminate_blue_instances_on_deployment_success {
+    action = "${var.terminate_blue_instances_on_deployment_success_action}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -17,3 +17,33 @@ variable "lifecycle_rule_enabled" {
   description = "enable / disable default lifecycle rule"
   default     = "true"
 }
+
+variable "deployment_option" {
+  description = "Indicates whether to route deployment traffic behind a load balancer."
+  default     = "WITH_TRAFFIC_CONTROL"
+}
+
+deployment_type {
+  description = "Indicates whether to run an in-place deployment or a blue/green deployment."
+  default     = "IN_PLACE"
+}
+
+variable "action_on_timeout" {
+  description = "When to reroute traffic from an original environment to a replacement environment in a blue/green deployment"
+  default     = "STOP_DEPLOYMENT"
+}
+
+variable "wait_time_in_minutes" {
+  description = "Wait time in minutes to wait more changuing the status of the deployment"
+  default     = "15"
+}
+
+variable "green_fleet_provisioning_option_action" {
+  description = "The method used to add instances to a replacement environment"
+  default     = "COPY_AUTO_SCALING_GROUP"
+}
+
+variable "terminate_blue_instances_on_deployment_success_action" {
+  description = "The action to take on instances in the original environment after a successful blue/green deployment."
+  default     = "TERMINATE"
+}
