115d77d530708764b0422af70f0487ffd7be79f5
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-09-30 20:43:06 +0000

    Duplicate

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 variable "orchestration" {}
 variable "environment" {}
 
-variable "deployment_config_name" {
-  default = "CodeDeployDefault.OneAtATime"
-}
-
 variable "autoscaling_groups" {}
 
 variable "compute_platform" {
